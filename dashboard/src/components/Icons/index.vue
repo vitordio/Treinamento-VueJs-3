f1ed51485f49d019ead31f60a686bbdd4d01ddf9
Vitor Dio <vitor.gracia@gmail.com>
2022-02-23 20:52:32 -0300

    Components de Icones e Loading

---
+++ dashboard/src/components/Icons/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="name" v-bind="$props"></component>
+</template>
+
+<script>
+import Loading from './Loading.vue'
+
+export default {
+  components: {
+    Loading
+  },
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
