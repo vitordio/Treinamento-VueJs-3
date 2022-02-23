abdb770033e18d33f20b1bcc2d2d4cd2e47b3512
Vitor Dio <vitor.gracia@gmail.com>
2022-02-23 20:51:00 -0300

    Configurações iniciais - Projeto Feedbacker - instalação, styles e fonts

---
+++ dashboard/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-multiple-template-root': 'off',
+    'valid-template-root': 'off',
+    'vue/valid-template-root': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
